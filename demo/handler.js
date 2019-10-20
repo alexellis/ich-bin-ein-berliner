446190c0c04ed041a9017aedfa9096523f10754d
Alex Ellis <alexellis2@gmail.com>
2019-10-20 10:19:30 +0100

    Test
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- demo/handler.js
+++ demo/handler.js
@@ -7,7 +7,9 @@ module.exports = (event, context) => {
   context
     .status(200)
     .headers({"Content-Type": "text/html"})
-    .succeed(`<html><body>
-    <p>Ich bin ein Berliner `+icon+`</p>
-    </body></html>`);
+    .succeed(`<html><head><style>body {font-family: Verdana;}</style></head>
+    <body>
+      <h2>Ich bin ein Berliner `+icon+`</h2>
+    </body>
+    </html>`);
 }
