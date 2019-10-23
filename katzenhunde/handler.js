ba048b08bba30ff26566f4dfada56c925a6c6e44
Alex Ellis <alexellis2@gmail.com>
2019-10-23 10:14:00 +0200

    Update handler.js

--- katzenhunde/handler.js
+++ katzenhunde/handler.js
@@ -1,15 +1,15 @@
 "use strict"
 
 module.exports = (event, context) => {
-  // let icon = "🐶";
-  let icon = "😸";
+  let y = "🐶";
+  let x = "😸";
 
   context
     .status(200)
     .headers({"Content-Type": "text/html"})
     .succeed(`<html><head><style>body {font-family: Verdana;}</style></head>
     <body>
-      <h2>Wir mag lieben `+icon+`</h2>
+      <h2>`+x+` machen glücklicher als `+y+`</h2>
     </body>
     </html>`);
 }
