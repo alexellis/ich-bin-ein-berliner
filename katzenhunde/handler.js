38ac4931a3cfea181ee65825f672c09fb0c471ee
Alex Ellis <alexellis2@gmail.com>
2019-10-23 10:17:21 +0200

    Update handler.js

--- katzenhunde/handler.js
+++ katzenhunde/handler.js
@@ -4,13 +4,13 @@ module.exports = (event, context) => {
   let y = "🐶";
   let x = "😸";
 
+  let color = process.env.color || "white";
   context
     .status(200)
     .headers({"Content-Type": "text/html"})
-    .succeed(`<html><head><style>body {font-family: Verdana;}</style></head>
+    .succeed(`<html><head><style>body { font-family: Verdana; background-color: `+color+`;}</style></head>
     <body>
       <h2>`+x+` machen glücklicher als `+y+`</h2>
     </body>
     </html>`);
 }
-
